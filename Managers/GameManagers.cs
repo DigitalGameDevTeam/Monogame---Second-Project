4d788a80ff595c38b5f72c7dd0485cda2b301bb4
joaoribeiro141516@gmail.com <joaoribeiro141516@gmail.com>
2024-05-15 11:36:37 +0100

    Edit "GameManager.cs" add try to use Content

--- Managers/GameManagers.cs
+++ Managers/GameManagers.cs
@@ -8,14 +8,15 @@ public class GameManager
 {
     public GameManager()
     {
-
+        _player = new(Globals.Content.Load<Texture2D>("player"), new(200, 200));
     }
     public void Update()
     {
         InputManager.Update();
+        _player.Update();
     }
     public void Draw()
     {
-
+        _player.Draw();
     }
 }
