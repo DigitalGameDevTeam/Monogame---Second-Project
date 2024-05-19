1bc807231b02fb9bd7c8ef49e7798d7d765ef620
Martim Moreira <98058202+Patoinks@users.noreply.github.com>
2024-05-19 15:39:53 +0100

    HighScore

--- GameStats.cs
+++ GameStats.cs
@@ -1,7 +1,9 @@
+using System;
+using System.IO;
+
 public class GameStats
 {
     private static GameStats _instance;
-    public  Player Player { get; set; }
     public static GameStats Instance
     {
         get
@@ -13,9 +15,49 @@ public class GameStats
     }
 
     public int Kills { get; set; }
+    public int HighScore { get; private set; }
+
+    private const string HighScoreFileName = "highscore.txt"; // File name to store high score
 
     private GameStats()
     {
         Kills = 0;
+        LoadHighScore(); // Load high score when the instance is created
+    }
+
+    public void UpdateHighScore()
+    {
+        if (Kills > HighScore)
+        {
+            HighScore = Kills;
+            SaveHighScore(); // Save high score if it's updated
+        }
+    }
+
+    private void LoadHighScore()
+    {
+        if (File.Exists(HighScoreFileName))
+        {
+            try
+            {
+                HighScore = Convert.ToInt32(File.ReadAllText(HighScoreFileName));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error loading high score: " + e.Message);
+            }
+        }
+    }
+
+    private void SaveHighScore()
+    {
+        try
+        {
+            File.WriteAllText(HighScoreFileName, HighScore.ToString());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error saving high score: " + e.Message);
+        }
     }
 }
